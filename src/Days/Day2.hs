1aa345df2c88cb55fa9dd4d6c04cdfd3dc8c1c46
Lukas <dev.lukas.huwald@gmail.com>
2018-12-02 23:49:03 +0100

    day 2

---
+++ src/Days/Day2.hs
@@ -0,0 +1,26 @@
+module Days.Day2(run) where
+
+import Data.List
+
+run :: String -> String
+run s = let hashes = lines s in
+    show (checksum hashes) ++ ", " ++ (solution hashes)
+
+checksum :: [String] -> Int
+checksum hashes = 
+    let twiceHashes  = filter (anyOccursTimes 2) hashes
+        thriceHashes = filter (anyOccursTimes 3) hashes in 
+            (length twiceHashes) * (length thriceHashes)
+
+anyOccursTimes :: Int -> String -> Bool
+anyOccursTimes times = any (== times) . occurCounts
+    where occurCounts = (map length) . group . sort
+
+solution :: [String] -> String
+solution hashes = head [equalChars s t | s <- hashes, t <- hashes, s < t, (countDifferences s t) == 1]
+
+equalChars :: String -> String -> String
+equalChars s t = [a | (a, b) <- zip s t, a == b]
+
+countDifferences :: String -> String -> Int
+countDifferences s t = length [a | (a, b) <- zip s t, not (a == b)]
--- src/Switcher.hs
+++ src/Switcher.hs
@@ -1,6 +1,8 @@
 module Switcher(runDay) where
 
 import Days.Day1
+import Days.Day2
 
 runDay :: Int -> String -> String
-runDay 1 = Days.Day1.run 
+runDay 1 = Days.Day1.run
+runDay 2 = Days.Day2.run 
