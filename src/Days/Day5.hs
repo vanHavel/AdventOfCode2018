3d23228a7f18af7904925e58c4b13ab8c9807029
Lukas <dev.lukas.huwald@gmail.com>
2018-12-12 23:00:26 +0100

    day5

---
+++ src/Days/Day5.hs
@@ -0,0 +1,15 @@
+module Days.Day5 where
+
+import Data.Char
+
+run :: String -> String
+run s = show (length $ reduce (init s) []) ++ ", " ++ show (bestLength $ init s)
+
+reduce :: String -> String -> String
+reduce [] ys = ys
+reduce (x:xs) [] = reduce xs [x]
+reduce (x:xs) (y:ys) | x /= y && toLower x == toLower y = reduce xs ys
+                     | otherwise                        = reduce xs (x:y:ys)
+
+bestLength :: String -> Int
+bestLength s = minimum [length $ reduce (filter (\c -> toLower c /= a) s) [] | a <- ['a'..'z']]
--- src/Switcher.hs
+++ src/Switcher.hs
@@ -4,9 +4,13 @@ import Days.Day1
 import Days.Day2
 import Days.Day3
 import Days.Day4
+import Days.Day5
+import Days.Day6
 
 runDay :: Int -> String -> String
 runDay 1 = Days.Day1.run
 runDay 2 = Days.Day2.run 
 runDay 3 = Days.Day3.run 
 runDay 4 = Days.Day4.run
+runDay 5 = Days.Day5.run
+runDay 6 = Days.Day6.run
